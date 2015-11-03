f78c41d5b7174d42032841b4eaaa12b644dc7093
Neel Mehta <neelmehta247@gmail.com>
2015-11-03 23:01:27 +0530

    Caching times to various tasks.

--- src/LocationNode.java
+++ src/LocationNode.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by neel on 27/10/15 at 11:37 PM.
  */
@@ -6,6 +8,8 @@ public class LocationNode {
     //Separate class created for location since the Task object would be obtained directly from the database
     //and so this class could be used to perform all the functions on the Task object without affecting it
     private Task task;
+    private ArrayList<LocationNode> locationNodes = new ArrayList<>();
+    private ArrayList<Double> times = new ArrayList<>();
 
     public LocationNode(Task task) {
         this.task = task;
@@ -16,17 +20,26 @@ public class LocationNode {
     }
 
     public double timeFrom(LocationNode location) {
-        //The time taken should be calculated using Android APIs to get transit time between 2 places
-        //For now, a time based on distance only is being sent assuming
-        double latDistance = Math.toRadians(task.latitude - location.getTask().latitude);
-        double lonDistance = Math.toRadians(task.longitude - location.getTask().longitude);
-        double a = (Math.sin(latDistance / 2) * Math.sin(latDistance / 2)) + (Math.cos(Math.toRadians(task.latitude)) *
-                Math.cos(Math.toRadians(location.getTask().latitude)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2));
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = c * 6371; //km
+        if (locationNodes.contains(location)) {
+            return times.get(locationNodes.indexOf(location));
+        } else {
+            //The time taken should be calculated using Android APIs to get transit time between 2 places
+            //For now, a time based on distance only is being sent assuming
+            double latDistance = Math.toRadians(task.latitude - location.getTask().latitude);
+            double lonDistance = Math.toRadians(task.longitude - location.getTask().longitude);
+            double a = (Math.sin(latDistance / 2) * Math.sin(latDistance / 2)) + (Math.cos(Math.toRadians(task.latitude)) *
+                    Math.cos(Math.toRadians(location.getTask().latitude)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2));
+            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            double distance = c * 6371; //km
 
-        //Returns time in seconds. The time taken to finish the task should also be in seconds.
-        return distance / 20; //Assuming 20 kmph. This will be different.
+            //Returns time in seconds. The time taken to finish the task should also be in seconds.
+            double time = distance / 20; //Assuming 20 kmph. This will be different.
+
+            locationNodes.add(location);
+            times.add(time);
+
+            return time;
+        }
     }
 
     public double timeTillFinishedFrom(LocationNode location) {
